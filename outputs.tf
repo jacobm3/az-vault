ba436262da9fc1575c84d9467863422d73e2b057
Jacob Martinson <jacobm3@gmail.com>
2020-01-10 16:57:37 -0600

    Add windows server.

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,3 @@ output "public_ip_address" {
   value = azurerm_public_ip.main.ip_address
 }
 
-# data "azurerm_public_ip" "main" {
-#   name                = "${azurerm_public_ip.main.name}"
-#   resource_group_name = "${azurerm_virtual_machine.main.resource_group_name}"
-# }
-
-# output "public_ip_address2" {
-#   value = "${data.azurerm_public_ip.main.ip_address}"
-# }
---
+++ windows-server.tf
@@ -0,0 +1,16 @@
+  module "windowsservers" {
+    source              = "Azure/vm/azurerm"
+    resource_group_name = azurerm_resource_group.main.name
+    location            = "westus2"
+    vm_hostname         = "${var.prefix}-win-server-vm"
+    admin_password      = "Com@#$%asdfADSFrd!"
+    public_ip_dns       = ["jmartinson-vault-win-server-ip"]
+    nb_public_ip        = "1"
+    remote_port         = "3389"
+    nb_instances        = "1"
+    vm_os_publisher     = "MicrosoftWindowsServer"
+    vm_os_offer         = "WindowsServer"
+    vm_os_sku           = "2012-R2-Datacenter"
+    vm_size             = "Standard_DS2_V2"
+    vnet_subnet_id      = azurerm_subnet.internal.id
+  }
