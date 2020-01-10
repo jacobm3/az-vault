3949075d9742940f41bef66c59e64ca696a2bc27
Jacob Martinson <jacobm3@gmail.com>
2020-01-10 15:14:41 -0600

    Add Vault server public IP

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-data "azurerm_public_ip" "main" {
-  name                = azurerm_public_ip.main.name
-  resource_group_name = azurerm_virtual_machine.main.resource_group_name
-}
-
 output "public_ip_address" {
-  value = data.azurerm_public_ip.main.ip_address
+  value = azurerm_public_ip.main.ip_address
 }
