f0fc0ae21ac92222760a51cd9f6eb569f15429f8
Jacob Martinson <jacobm3@gmail.com>
2020-01-12 01:01:39 -0600

    Add mysql server.

--- mysql.tf
+++ mysql.tf
@@ -1,7 +1,7 @@
 resource "azurerm_mysql_server" "mysql" {
   name                = "mysql-server-1"
-  location            = "${azurerm_resource_group.main.location}"
-  resource_group_name = "${azurerm_resource_group.main.name}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
   sku {
     name     = "B_Gen5_2"
